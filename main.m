83912119c67c9fbaf3f87db573e31febe23210c1
Jjgod Jiang <gzjjgod@gmail.com>
2009-05-04 16:30:14 +0800

    Add project files

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WaveView
+//
+//  Created by Jjgod Jiang on 5/4/09.
+//  Copyright Jjgod Jiang 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
